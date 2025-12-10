7cb8d021e55020bc29362ed411ae3f9ee10438b3
dm1299 <dm1299onion@gmail.com>
2025-12-10 16:30:45 +0800

    feat: init

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,11 +1,17 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  "/md/resume",
-  {
-    text: "开发日志",
-    icon: "book",
-    link: "/md/dev-log/day01.md",
-  },
+    "/",
+    //"/md/resume",
+    {
+        text: "开发日志",
+        icon: "book",
+        link: "/md/dev-log/day01.md",
+    },
+    {
+        text: "并发编程",
+        icon: "gears",
+        link: "/md/并发编程",
+        line: "/md/并发编程/day01.md"
+    },
 ]);
